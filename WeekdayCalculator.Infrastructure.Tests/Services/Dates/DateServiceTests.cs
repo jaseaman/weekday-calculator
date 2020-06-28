f50debd48880954698d54a9e138aba845b391673
James Seaman <james.al.seaman@gmail.com>
2020-06-28 12:23:49 +1000

    INITIAL: Added configuration for NUnit testing for TDD

---
+++ WeekdayCalculator.Infrastructure.Tests/Services/Dates/DateServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using WeekdayCalculator.Core.Services.Dates;
+
+namespace WeekdayCalculator.Core.Tests.Services.Dates
+{
+    [TestFixture]
+    public class DateServiceTests
+    {
+        private IDateService _dateService;
+
+        private static readonly object[] TestWeekdaysCalculationTestCases =
+        {
+            // - Thu 7/8/2014 to Mon 11/8/2014 should return 1
+            new object[] {new DateTime(2014, 8, 7), new DateTime(2014, 08, 11), 1},
+            // - Wed 13/8/2014 to Thu 21/8/2014 should return 5
+            new object[] {new DateTime(2014, 8, 13), new DateTime(2014, 8, 21), 5}
+        };
+        
+        [TestCaseSource(nameof(TestWeekdaysCalculationTestCases))]
+        public async Task TestWeekdaysCalculation(DateTime from, DateTime to, long expectedResult)
+        {
+            
+        }
+    }
+}
